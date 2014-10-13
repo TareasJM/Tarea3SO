152703bd31f1f3fed5d8d8bb75ab8814534cef7c
Salinas <marco.salinas.12@sansano.usm.cl>
2014-10-13 19:32:12 -0300

    Tarea OK

--- Car.java
+++ Car.java
@@ -4,66 +4,212 @@ import java.awt.event.KeyEvent;
 public class Car implements Runnable{
 	private Game g;
 	private int[] pos;
+	private int[] initPos;
 	private char dir;
 	private boolean player;
+	private char playerChar;
+	private int playerInt;
 
-	public Car(Game g, int[] pos, boolean player){
+	public Car(Game g, int[] pos, boolean player, int n){
 		this.g = g;
 		this.pos = pos;
+		this.initPos = new int[2];
+		this.initPos[0] = pos[0];
+		this.initPos[1] = pos[1];
 		this.dir = 'U';
 		this.player = player;
+		this.playerInt = n;
+		if (player) {
+			this.playerChar = 'V';
+		}
+		else{
+			this.playerChar = 'E';
+		}
 	}
 
 	public void run(){
-		while(drive() == 0){
+		while(drive() == 0 && g.getLife() > 0 && g.getFlags() > 0){
+      		if (this.g.getReset(this.playerInt)) {
+      			resetPos();
+        		this.g.setReset(this.playerInt,false);
+      		}	
 			try {
 				Thread.sleep(300);
 			} catch (Exception e) {
 		    	System.out.println(e);
 			}
 		}
+		System.out.println("Termino "+this.playerInt);
 	}
 
 	private int drive(){
-		char prev = ' ';
+		if (!player) {
+			autoDrive();
+			return 0;			
+		}
+		char next = '#';
+		int newx, newy;
+		newx = pos[1];
+		newy = pos[0];
+		
 		synchronized(g.map){
-			if (dir == 'U') {
-				prev = g.map[pos[0]-1][pos[1]];
-				g.map[pos[0]-1][pos[1]] = g.map[pos[0]][pos[1]];
-				g.map[pos[0]][pos[1]] = ' '; 
-				pos[0]--;
-			}
-			else if (dir == 'D') {
-				prev = g.map[pos[0]+1][pos[1]];
-				g.map[pos[0]+1][pos[1]] = g.map[pos[0]][pos[1]];
-				g.map[pos[0]][pos[1]] = ' '; 
-				g.map[pos[0]+1][pos[1]] = 'V'; 
-				pos[0]++;
-			}
-			else if (dir == 'L') {
-				prev = g.map[pos[0]][pos[1]-1];
-				g.map[pos[0]][pos[1]-1] = g.map[pos[0]][pos[1]];
-				g.map[pos[0]][pos[1]] = ' '; 
-				g.map[pos[0]][pos[1]-1] = 'V'; 
-				pos[1]--;
-			}
-			else if (dir == 'R') {
-				prev = g.map[pos[0]][pos[1]+1];
-				g.map[pos[0]][pos[1]+1] = g.map[pos[0]][pos[1]];
-				g.map[pos[0]][pos[1]] = ' '; 
-				g.map[pos[0]][pos[1]+1] = 'V'; 
-				pos[1]++;
-			}
+			while(next == '#')
+			{	
+				newx = pos[1];
+				newy = pos[0];
+				if (dir == 'U') {
+					next = g.map[pos[0]-1][pos[1]];
+					newy--;
+					
+				}
+				else if (dir == 'D') {
+					next = g.map[pos[0]+1][pos[1]];
+					newy++;
+				}
+				else if (dir == 'L') {
+					next = g.map[pos[0]][pos[1]-1];
+					newx--;
+				}
+				else if (dir == 'R') {
+					next = g.map[pos[0]][pos[1]+1];
+					newx++;
+				}
 
-			if (prev == '#') {
-				g.decLife();
-				return 1;
+				if (next == '#') {
+					if(dir == 'L')
+					{
+						this.setDir('U');
+					}
+					else if(dir == 'U')
+					{
+						this.setDir('R');
+					}
+					else if(dir == 'R')
+					{
+						this.setDir('D');
+					}
+					else if(dir == 'D')
+					{
+						this.setDir('L');
+					}
+				}
 			}
+			if(next == 'B')
+			{
+				g.decFlags();
+				
+			}
+			else if(next == 'O' || next == 'E')
+			{	
+				g.reset();
+				resetPos();
+				g.decLife();
+				return 0;
+			}
+			g.map[pos[0]][pos[1]] = ' ';
+			pos[1] = newx;
+			pos[0] = newy;
+			g.map[newy][newx] = playerChar;
+			
+			
 		}
 		return 0;
 	}
 
+	public void resetPos(){
+		synchronized(g.map){
+			g.map[pos[0]][pos[1]] = ' ';
+			this.pos[0] = this.initPos[0];
+			this.pos[1] = this.initPos[1];
+			g.map[pos[0]][pos[1]] = playerChar;
+		}
+	}
+
 	public void setDir(char dir){
 		this.dir = dir;
+	}
+	public void autoDrive(){
+		char next = '#';
+		int newx, newy;
+		newx = pos[1];
+		newy = pos[0];
+		int[] posV = new int[2];
+		posV = this.g.getPosV();
+		if(posV[0] > this.pos[0] )
+		{
+			setDir('D');
+		}
+		else if(posV[0] < this.pos[0] )
+		{
+			setDir('U');
+		}
+		else if(posV[1] < this.pos[1] )
+		{
+			setDir('L');
+		}
+		else if(posV[1] > this.pos[1] )
+		{
+			setDir('R');
+		}
+		synchronized(g.map){
+			while(next == '#' || next == 'B' || next ==  'O' || next ==  'E')
+			{	
+				newx = pos[1];
+				newy = pos[0];
+				if (dir == 'U') {
+					next = g.map[pos[0]-1][pos[1]];
+					newy--;
+					
+				}
+				else if (dir == 'D') {
+					next = g.map[pos[0]+1][pos[1]];
+					newy++;
+				}
+				else if (dir == 'L') {
+					next = g.map[pos[0]][pos[1]-1];
+					newx--;
+				}
+				else if (dir == 'R') {
+					next = g.map[pos[0]][pos[1]+1];
+					newx++;
+				}
+
+				if (next == '#' || next == 'B' || next ==  'O' || next ==  'E') {
+					if(dir == 'L')
+					{
+						this.setDir('U');
+					}
+					else if(dir == 'U')
+					{
+						this.setDir('R');
+					}
+					else if(dir == 'R')
+					{
+						this.setDir('D');
+					}
+					else if(dir == 'D')
+					{
+						this.setDir('L');
+					}
+				}
+			}
+			if(next == 'V')
+			{
+				this.g.reset();
+				this.g.decLife();
+				return;
+			}
+			g.map[pos[0]][pos[1]] = ' ';
+			pos[1] = newx;
+			pos[0] = newy;
+			g.map[newy][newx] = playerChar;
+			
+			
+		}
+	}
+
+	public int[] getPos()
+	{
+		return this.pos;
 	}
 }
--- Game.java
+++ Game.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyListener;
 
 public class Game extends JFrame implements KeyListener{
   char map[][] = new char[20][40];
+  Boolean reset[];
   Integer life;
   Integer flags;
   int posV[];
@@ -14,13 +15,16 @@ public class Game extends JFrame implements KeyListener{
   JPanel main;
   JLabel blocks[];
   Car carV;
+  Car carE[];
   
   public Game(char[][] map, int[] posV, int[][] posE) throws IOException{
     this.map = map;
     this.posV = posV;
     this.posE = posE;
-    this.life = 1;
-    this.flags = 5;
+    this.life = 2;
+    this.flags = 4;
+    this.reset = new Boolean[5];
+    this.reset[0] = this.reset[1] = this.reset[2] = this.reset[3] = this.reset[4] = false;
     blocks = new JLabel[800];
     addKeyListener(this);
     setTitle("Rali equis");
@@ -31,8 +35,8 @@ public class Game extends JFrame implements KeyListener{
     main.setLayout(new GridLayout(20,40)); //FILA / COLUMNAS
     for (int i=0; i < 20; i++ ) {
       for (int j=0; j < 40; j++ ) {
-        blocks[i*20 + j] = new JLabel(map[i][j]+"");
-        main.add(blocks[i*20 + j]);
+        blocks[i*40 + j] = new JLabel(map[i][j]+"");
+        main.add(blocks[i*40 + j]);
       }
     } 
     add(main);
@@ -41,13 +45,26 @@ public class Game extends JFrame implements KeyListener{
   }
 
   public int play() {
-    this.carV = new Car(this, posV, true);
+    this.carV = new Car(this, posV, true,0);
+    this.carE = new Car[4];
+    this.carE[0] = new Car(this, posE[0], false,1);
+    this.carE[1] = new Car(this, posE[1], false,2);
+    this.carE[2] = new Car(this, posE[2], false,3);
+    this.carE[3] = new Car(this, posE[3], false,4);
     Thread vThread = new Thread(carV);
+    Thread e0Thread = new Thread(carE[0]);
+    Thread e1Thread = new Thread(carE[1]);
+    Thread e2Thread = new Thread(carE[2]);
+    Thread e3Thread = new Thread(carE[3]);
     vThread.start();
+    e0Thread.start();
+    e1Thread.start();
+    e2Thread.start();
+    e3Thread.start();
 
-    while(getLife()>0){
+    while(getLife()>0 && getFlags()>0){
+
       try {
-        Runtime.getRuntime().exec("clear");
         printMap();
         Thread.sleep(150);
       } catch (Exception e) {
@@ -55,6 +72,10 @@ public class Game extends JFrame implements KeyListener{
       }
     }
 
+    System.out.println("salio while");
+    setVisible(false);
+    dispose();
+    
     return 0;
   }
 
@@ -62,6 +83,7 @@ public class Game extends JFrame implements KeyListener{
     char[][] m = getMap();
     int l = getLife();
     int f = getFlags();
+    setTitle("Rali equis     "  + "Banderas = " + f + " Vidas = " + l);
     //System.out.println("Vidas:"+Integer.toString(life)+" | Banderas:"+Integer.toString(flags));
     //for (int i=0; i < m.length; i++ ) {
     //  for (int j=0; j < m[i].length; j++ ) {
@@ -71,9 +93,10 @@ public class Game extends JFrame implements KeyListener{
     //}
     for (int i=0; i < 20; i++ ) {
       for (int j=0; j < 40; j++ ) {
-        blocks[i*20 + j].setText(m[i][j]+"");
+        blocks[i*40 + j].setText(m[i][j]+"");
       }
     }  
+    pack();
   }
 
   public char[][] getMap(){
@@ -106,6 +129,26 @@ public class Game extends JFrame implements KeyListener{
     }
   }
 
+  public void reset(){
+   
+    synchronized(this.reset){
+
+     this.reset[0] = this.reset[1] = this.reset[2] = this.reset[3] = this.reset[4] = true;
+    }
+  }
+  public void setReset(int n, Boolean r){
+    synchronized(this.reset){
+      this.reset[n] = r;
+    }
+  }
+
+
+  public Boolean getReset(int n){
+    synchronized(this.reset){
+      return this.reset[n];
+    }
+  }
+
   public void keyTyped(KeyEvent e) {
   }
 
@@ -121,5 +164,10 @@ public class Game extends JFrame implements KeyListener{
       this.carV.setDir('D');
     else if(e.getKeyCode()== KeyEvent.VK_UP)
       this.carV.setDir('U');
+  }
+
+  public int[] getPosV()
+  {
+    return this.carV.getPos();
   }
 } 
